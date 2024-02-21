73dc3125aa316029613b711f64bb811cf4d95cf7
Ivy1610 <8682@holbertonstudent.com>
2024-02-21 15:52:56 +0100

    Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,15 +2,14 @@
 /**
  * main - prints all the numbers of base 16 in lowercase
  *
- * Retune: Always 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	int num;
 	char letter;
 
-	for (num = 0; num < 10 ; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
@@ -20,4 +19,3 @@ int main(void)
 
 	return (0);
 }
-
