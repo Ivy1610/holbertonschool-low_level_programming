24e2bf1643e868b6822c100f5b2e167b5fbe8235
Ivy1610 <8682@holbertonstudent.com>
2024-02-21 18:18:05 +0100

    9. Patience, persistence and perspiration

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,13 +8,17 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-		if (num != '9')
+		if (num < 9)
 		{
 			putchar(',');
 			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
 		}
 	}
 	return (0);
