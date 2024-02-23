07ab7c2b11c22f4f9712da845fb8f9cb54901cfb
Ivy1610 <8682@holbertonstudent.com>
2024-02-23 10:37:53 +0100

    0. _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - prints _putchar, followed by a new line.
  *
@@ -6,14 +7,14 @@
  */
 int main(void)
 {
-	int i;
-	char c[8] = "_putchar";
-	{
-		for (i = 0; i < 8; i++)
-		{
-			_putchar(c[i]);
-		}
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
 		_putchar('\n');
-		return (0);
-	}
+		return ('0');
 }
