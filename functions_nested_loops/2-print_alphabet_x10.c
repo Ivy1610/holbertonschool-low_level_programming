8fa20bc622ed9102dc31e87637c9e3b54f7fecb6
Ivy1610 <8682@holbertonstudent.com>
2024-02-23 12:22:44 +0100

    c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char c[27] = "abcdefghijklmnopqrstuvwxyz";
+	{
+	for (n = 0; n <= 26; n++)
+	{
+		putchar(abc[n]);
+	}
+	putchar('\n');
+	}
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -10,3 +10,14 @@ int main(void)
  	print_alphabet();
        return (0);
 }
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+       	print_alphabet_x10();
+	return (0);
+}
