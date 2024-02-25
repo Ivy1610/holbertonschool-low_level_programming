69621fbf08593b65b29bc3b5029e0b2dd293b81e
Ivy1610 <8682@holbertonstudent.com>
2024-02-25 12:54:49 +0100

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *
+ * @i : number
+ * @j : number
+ *
+ * Return: int.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
