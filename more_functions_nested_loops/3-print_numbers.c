c6a8a6ec1938b1ddfff0130d38d8a8300a813686
Ivy1610 <8682@holbertonstudent.com>
2024-02-26 06:22:15 +0100

    3. The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_numbers -  function that prints the numbers
- * from 0 to 9, followed by a new line.
- * @a : variables
- *
- *Return: Always 0.
+ * print_numbers - function that prints the numbers from 0 to 9
  */
 void print_numbers(void)
 {
-	int c;
+	int c
 
 	for (c = 48; c < 58; c++)
 	{
