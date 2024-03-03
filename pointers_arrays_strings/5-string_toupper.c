cb22f4513a9910fddebcdaa8495699e3bb1f8e59
Ivy1610 <8682@holbertonstudent.com>
2024-03-03 22:38:15 +0100

    5. Always look up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - change all lowercase letters of a string to uppercase
+ * @s: srting
+ *
+ * Return: pointer to the resulting string
+ */
+char *string_toupper(char *s)
+{
+	char *c;
+
+	for (c = s; *c; ++c)
+	{
+		if ('a' <= *c && *c <= 'z')
+			*c -= 'a' - 'A';
+	}
+	return (s);
+}
+
+
