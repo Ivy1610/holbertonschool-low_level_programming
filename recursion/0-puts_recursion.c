a29a74e284a5fd0eab63215f3a489d139951a4a8
Ivy1610 <8682@holbertonstudent.com>
2024-03-06 14:00:04 +0100

    0. She locked away a secret

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != 0)
 	{
-		printf("\n");
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	printf("%c", *s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar('\n');
+	}
 }
