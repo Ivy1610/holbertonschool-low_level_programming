1d1ec331a1b783201468f252940153a76a4bb657
Ivy1610 <8682@holbertonstudent.com>
2024-03-08 21:54:26 +0100

    my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,14 +3,11 @@
 /**
  * main - print name
  *@argc: int.
- *@argv: char.
+ *@argv: array 
  * Return: integer
  */
 int main(int argc, char *argv[])
 {
-       if (argc >= 1)
-	{
-		printf("%s\n", argv[0]);
-	}
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
