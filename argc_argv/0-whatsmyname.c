a3dd0669060977693901cefce294e3294d1f6a97
Ivy1610 <8682@holbertonstudent.com>
2024-03-08 15:50:05 +0100

    0. It ain't what they call you

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,15 +4,13 @@
  * main - print name
  *@argc: int.
  *@argv: char.
- * Return: Always 0.
+ * Return: integer
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	
-	for (i = 0; i < argc ; i++)
+       if (argc >= 1)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
