d7807c19ab40cbdf2d5a06451f314b4ad08d1340
Ivy1610 <8682@holbertonstudent.com>
2024-03-08 16:10:03 +0100

    1.silence is argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point main
+ * @argc: the number of arguments
+ * @argv: the pointer containt an array of strings
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[]
+__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
