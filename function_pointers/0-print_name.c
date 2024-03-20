9af5c550254e2af6acee81f9fabfba6e97cbe19c
Ivy1610 <8682@holbertonstudent.com>
2024-03-20 13:08:53 +0100

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - function that prints a name
- * @name: string to add
- * @f: pointer to function
- * Return: Nothing
+ * @name: name
+ * @f: function to print
  */
 void print_name(char *name, void (*f)(char *))
 {
