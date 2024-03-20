8c705975712a6ea1b9c725a2dabde436ab50078e
Ivy1610 <8682@holbertonstudent.com>
2024-03-20 12:10:03 +0100

    contain all the function prototypes and data structures used by the program

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,10 +1,7 @@
 #ifndef _CALC_H_
 #define _CALC_H_
-
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * struct op - Struct op
+ * struct op - A struct op
  *
  * @op: The operator
  * @f: The function associated
