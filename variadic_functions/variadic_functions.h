38b1b1b2d3c928158e6af1a8872658b34c44cc80
Ivy1610 <8682@holbertonstudent.com>
2024-03-22 09:43:37 +0100

    variadic function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef _VARIABLE_NUM_
-#define _VARIABLE_NUM_
+#ifndef _VARIADIC_NUM_
+#define _VARIADIC_NUM_
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -7,4 +8,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * srtuct format - Data type of a format
+ * @op: format
+ * @f: function
+ */
+
+typedef struct format
+{
+	 char *op;
+	 void (*f)(va_list all);
+} f;
 #endif
