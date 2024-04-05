863ddf0b78335913f3f5969dda98beed2524e460
Ivy1610 <8682@holbertonstudent.com>
2024-04-05 09:43:39 +0200

    Write a function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,25 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * _atoi - converti une chaine en entier
- * @s: 
+ * _atoi - function that convert a string to an integer.
+ * @s: pointer
+ * Return: restultat of the conversion
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
