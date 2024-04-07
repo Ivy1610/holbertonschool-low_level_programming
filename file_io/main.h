e7b8dc0179bbc8b7b433990b716c404db726a136
Ivy1610 <8682@holbertonstudent.com>
2024-04-08 01:29:48 +0200

    rajout de prototype

--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,20 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
 
+#define BUFFER_SIZE 1024
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error_and_exit(int exit_code, const char *message);
+int main(int argc, char *argv[]);
 
 #endif
